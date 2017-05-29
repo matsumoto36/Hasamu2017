a812d4e7e0ee9a93626387a52aa9b029e4c2117f
SUNRISE <hgadkuhin@gmail.com>
2017-05-29 09:39:15 +0900

    あついブロックスクリプトちゃん

---
+++ Assets/Gimmick/PieceFire.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceFire : MonoBehaviour
+{
+    public Vector2 point;
+
+	// Use this for initialization
+	void Start ()
+    {
+        		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
