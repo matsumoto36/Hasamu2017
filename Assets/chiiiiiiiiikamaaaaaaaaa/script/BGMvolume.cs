08ecd195b9344f0d32831077e5c5fffca130896a
eru <eru.birdsea@gmail.com>
2017-05-31 14:18:21 +0900

    BGMとSEの音量調節

---
+++ Assets/chiiiiiiiiikamaaaaaaaaa/script/BGMvolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMvolume : MonoBehaviour {
+
+	[SerializeField]
+	UnityEngine.Audio.AudioMixer mixer;
+	public float BGMVolume
+	{
+		set {
+			Debug.Log(value);
+			mixer.SetFloat("BGMVolume", value);
+		}
+	}
+		
+	}
