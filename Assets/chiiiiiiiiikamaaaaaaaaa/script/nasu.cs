d7025b131a38a7fcccc1f65b3fd5c0772f89b6b9
eru <eru.birdsea@gmail.com>
2017-06-07 13:43:01 +0900

    ポーズ機能

--- Assets/chiiiiiiiiikamaaaaaaaaa/script/nasu.cs
+++ Assets/chiiiiiiiiikamaaaaaaaaa/script/nasu.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// パズルの進行管理をする
-/// </summary>
-public class GameManager : MonoBehaviour {
+public class nasu : MonoBehaviour {
 
-	
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		transform.Rotate(new Vector3(0, 0, 5));
 	
 	}
+
+
+	
 }
