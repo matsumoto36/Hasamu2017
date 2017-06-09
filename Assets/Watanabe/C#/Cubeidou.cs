28b2f565c986ebc1f93a6981101908a53a5396fc
Game <nkc.hayata.watanabe@gmail.com>
2017-06-09 18:23:06 +0900

    time 更新

---
+++ Assets/Watanabe/C#/Cubeidou.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubeidou : MonoBehaviour {
+    private bool m_isVisibleTimer = false;
+    void Start()
+    {
+        Invoke("DelayMethod", 3.0f);
+    }
+    void FixedUpdate()
+    {
+        if (m_isVisibleTimer)
+        {
+            float x = Input.GetAxis("Horizontal");
+
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+            rigidbody.AddForce(x * 10, 0, 0);
+        }
+    }
+    public void DelayMethod()
+    {
+        m_isVisibleTimer = true;
+    }
+}
