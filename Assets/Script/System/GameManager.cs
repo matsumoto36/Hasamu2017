a2c2e656ff8abb56527d94db01da8659e6e18877
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2017-06-11 22:53:14 +0900

    ゲームフロー準備

--- Assets/Script/System/GameManager.cs
+++ Assets/Script/System/GameManager.cs
@@ -7,14 +7,53 @@ using UnityEngine;
 /// </summary>
 public class GameManager : MonoBehaviour {
 
-	
-	// Use this for initialization
-	void Start () {
-	
+	void Awake() {
+		GameInitiarize();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/// <summary>
+	/// ゲームの初期設定
+	/// </summary>
+	static void GameInitiarize() {
+
+		//音設定読み込み
+		FindObjectOfType<AudioManager>().Initiarize();
+
+		//マップチップのロード
+		FindObjectOfType<ResourceLoader>().LoadChip();
+
+		//後にCSVから読み込む
+		int[,] map = new int[,] {
+			{1, 1, 1, 1, 1, 1, 2, 2, 2},
+			{1, 0, 0, 0, 3, 0, 0, 0, 1},
+			{1, 0, 0, 0, 4, 5, 0, 0, 1},
+			{1, 0, 0, 0, 3, 4, 5, 0, 1},
+			{1, 0, 0, 0, 0, 0, 0, 0, 1},
+			{1, 1, 1, 1, 1, 1, 1, 1, 1},
+		};
+
+		//ステージの生成
+		StageGenerator.GenerateMap(map);
+	}
+
+	/// <summary>
+	/// ゲームを開始する
+	/// </summary>
+	public void GameStart() {
+
+	}
+
+	/// <summary>
+	/// ゲームオーバー
+	/// </summary>
+	public void GameOver() {
+
+	}
+
+	/// <summary>
+	/// ゲームクリア
+	/// </summary>
+	public void GameClear() {
+
 	}
 }
