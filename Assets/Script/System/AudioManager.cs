ef8e15e0325c935be2b7fc922cec3a9d5cb9dbdb
Matsumoto <nkc.ryo.matsumoto@gmail.com>
2017-06-22 13:01:02 +0900

    BGMに、フェードイン・フェードアウトの機能を追加

--- Assets/Script/System/AudioManager.cs
+++ Assets/Script/System/AudioManager.cs
@@ -33,7 +33,8 @@ public class AudioManager : MonoBehaviour {
 	static AudioMixer mixer;			//ミキサー
 	static AudioClip[] SEclips;			//再生用リスト
 	static AudioClip[] BGMclips;		//再生用リスト
-	static AudioSource nowPlayingBGM;	//現在再生されているBGM
+	static AudioSource nowPlayingBGM;   //現在再生されているBGM
+	static BGMType latestPlayBGMType;	//再生されているBGMの種類
 
 	void Awake() {
 
@@ -96,10 +97,10 @@ public class AudioManager : MonoBehaviour {
 
 		AudioSource src = new GameObject("[Audio BGM - " + type.ToString() + "]").AddComponent<AudioSource>();
 		src.transform.SetParent(myManager.transform);
-		src.PlayOneShot(BGMclips[(int)type], vol);
+		src.clip = BGMclips[(int)type];
+		src.volume = vol;
 		src.outputAudioMixerGroup = mixer.FindMatchingGroups("BGM")[0];
-
-		nowPlayingBGM = src;
+		src.Play();
 
 		if(isLoop) {
 			src.loop = true;
@@ -107,5 +108,77 @@ public class AudioManager : MonoBehaviour {
 		else {
 			Destroy(src.gameObject, BGMclips[(int)type].length + 0.1f);
 		}
+
+		nowPlayingBGM = src;
+		latestPlayBGMType = type;
+	}
+
+	/// <summary>
+	/// BGMをフェードインさせる
+	/// </summary>
+	/// <param name="fadeTime">フェードする時間</param>
+	/// <param name="type">新しいBGMのタイプ</param>
+	/// <param name="vol">新しいBGMの大きさ</param>
+	/// <param name="isLoop">新しいBGMがループするか</param>
+	public static void FadeIn(float fadeTime, BGMType type, float vol, bool isLoop) {
+		myManager.StartCoroutine(FadeInAnim(fadeTime, type, vol, isLoop));
+	}
+	static IEnumerator FadeInAnim(float fadeTime, BGMType type, float vol, bool isLoop) {
+
+		//初期設定
+		AudioSource src = new GameObject("[Audio BGM - " + type.ToString() + " - FadeIn ]").AddComponent<AudioSource>();
+		src.transform.SetParent(myManager.transform);
+		src.clip = BGMclips[(int)type];
+		src.volume = 0;
+		src.outputAudioMixerGroup = mixer.FindMatchingGroups("BGM")[0];
+		src.Play();
+
+		//フェードイン
+		float t = 0;
+		while(t < 1.0f) {
+			t += Time.deltaTime / fadeTime;
+			src.volume = t * vol;
+			yield return null;
+		}
+
+		src.volume = vol;
+		src.name = "[Audio BGM - " + type.ToString() + "]";
+
+		if(nowPlayingBGM) Destroy(nowPlayingBGM.gameObject);
+
+		if(isLoop) {
+			src.loop = true;
+		}
+		else {
+			Destroy(src.gameObject, BGMclips[(int)type].length + 0.1f);
+		}
+
+		nowPlayingBGM = src;
+	}
+
+	/// <summary>
+	/// BGMをフェードアウトさせる
+	/// </summary>
+	/// <param name="fadeTime">フェードする時間</param>
+	public static void FadeOut(float fadeTime) {
+		myManager.StartCoroutine(FadeOutAnim(fadeTime));
+	}
+	static IEnumerator FadeOutAnim(float fadeTime) {
+
+		//初期設定
+		AudioSource src = nowPlayingBGM;
+		src.name = "[Audio BGM - " + latestPlayBGMType.ToString() + " - FadeOut ]";
+		nowPlayingBGM = null;
+
+		//フェードアウト
+		float t = 0;
+		float vol = src.volume;
+		while(t < 1.0f) {
+			t += Time.deltaTime / fadeTime;
+			src.volume = (1 - t) * vol;
+			yield return null;
+		}
+
+		yield return null;
 	}
 }
