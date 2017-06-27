e6a1a71ba1962c9e0dc39873d51443c91c0688c6
eru <eru.birdsea@gmail.com>
2017-06-27 14:53:11 +0900

    BGM/SEのバグ

--- Assets/chiiiiiiiiikamaaaaaaaaa/script/BGMvolume.cs
+++ Assets/chiiiiiiiiikamaaaaaaaaa/script/BGMvolume.cs
@@ -1,17 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BGMvolume : MonoBehaviour {
-
+	
 	[SerializeField]
 	UnityEngine.Audio.AudioMixer mixer;
+
+	void Start()
+	{
+		float bgmVolume;
+		mixer.GetFloat("BGMVolume", out bgmVolume);
+		GetComponent<Slider>().value = bgmVolume;
+			}
+
 	public float BGMVolume
 	{
 		set {
 			Debug.Log(value);
 			mixer.SetFloat("BGMVolume", value);
 		}
+		
 	}
 		
 	}
--- Assets/chiiiiiiiiikamaaaaaaaaa/script/SEVolume.cs
+++ Assets/chiiiiiiiiikamaaaaaaaaa/script/SEVolume.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SEVolume : MonoBehaviour {
 
 	[SerializeField]
 	UnityEngine.Audio.AudioMixer mixer;
+	void Start()
+	{
+		float seVolume;
+		mixer.GetFloat("SEVolume", out seVolume);
+		GetComponent<Slider>().value = seVolume;
+	}
 	public float SEvolume
 	{
 		set {
